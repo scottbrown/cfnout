8bc797b0c39ef741fe1067c5f353311ff66df261
Scott Brown <scott@justplainsimple.com>
2019-09-28 20:01:35 -0700

    Refactoring

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// The main entry into the application
 package main
 
 import (
@@ -12,8 +13,10 @@ import (
 	"github.com/urfave/cli"
 )
 
+// A container of all the environment configuration for this CLI app
 var env CliEnv
 
+// Prepares this CLI application with flags, usage text, etc.
 func setup(app *cli.App) {
 	app.Name = AppName
 	app.Copyright = AppCopyright
@@ -21,11 +24,6 @@ func setup(app *cli.App) {
 	app.Version = AppVersion
 	app.Usage = "Displays all outputs in a CloudFormation stack given the names of the outputs, whether they are exported or not."
 	app.ArgsUsage = "OUTPUT1 ..."
-}
-
-func main() {
-	app := cli.NewApp()
-	setup(app)
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -47,52 +45,76 @@ func main() {
 		},
 	}
 
-	app.Action = func(c *cli.Context) error {
-		if c.NArg() == 0 {
-			cli.ShowAppHelpAndExit(c, 1)
-		}
+	app.Action = start
+}
 
-		if err := env.Validate(); err != nil {
-			return err
-		}
-
-		config, err := external.LoadDefaultAWSConfig(
-			external.WithSharedConfigProfile(env.AWSProfile),
-			external.WithRegion(env.AWSRegion),
-		)
-		if err != nil {
-			return err
-		}
-
-		client := cloudformation.New(config)
-
-		input := cloudformation.DescribeStacksInput{
-			StackName: aws.String(env.StackName),
-		}
-
-		req := client.DescribeStacksRequest(&input)
-		res, err := req.Send(context.TODO())
-		if err != nil {
-			return err
-		}
-
-		nameMap := make(map[string]struct{}, c.NArg())
-		for _, a := range c.Args() {
-			nameMap[a] = struct{}{}
-		}
-
-		for _, s := range res.Stacks {
-			for _, o := range s.Outputs {
-				if _, ok := nameMap[*o.OutputKey]; ok {
-					fmt.Println(*o.OutputValue)
-				}
-			}
-		}
-
-		return nil
-	}
+// Main entry into the CLI app.
+func main() {
+	app := cli.NewApp()
+	setup(app)
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Converts the args (which are the names of the outputs in a CFN stack)
+// given by the user in the CLI to a map of those same names (since they
+// should be unique).  This allows us to easily check if they exist in
+// the stack's outputs.
+func argsToNameMap(c *cli.Context) map[string]struct{} {
+	nameMap := make(map[string]struct{}, c.NArg())
+	for _, a := range c.Args() {
+		nameMap[a] = struct{}{}
+	}
+
+	return nameMap
+}
+
+// Returns the stack by its name that was given in the environment
+func stack() (cloudformation.Stack, error) {
+	config, err := external.LoadDefaultAWSConfig(
+		external.WithSharedConfigProfile(env.AWSProfile),
+		external.WithRegion(env.AWSRegion),
+	)
+
+	client := cloudformation.New(config)
+
+	input := cloudformation.DescribeStacksInput{
+		StackName: aws.String(env.StackName),
+	}
+
+	req := client.DescribeStacksRequest(&input)
+	res, err := req.Send(context.TODO())
+	if err != nil {
+		return cloudformation.Stack{}, err
+	}
+
+	return res.Stacks[0], nil
+}
+
+// The meat of the application
+func start(c *cli.Context) error {
+	if c.NArg() == 0 {
+		cli.ShowAppHelpAndExit(c, 1)
+	}
+
+	nameMap := argsToNameMap(c)
+
+	if err := env.Validate(); err != nil {
+		return err
+	}
+
+	stack, err := stack()
+	if err != nil {
+		return err
+	}
+
+	for _, i := range stack.Outputs {
+		if _, ok := nameMap[*i.OutputKey]; ok {
+			fmt.Println(*i.OutputValue)
+		}
+	}
+
+	return nil
 }
